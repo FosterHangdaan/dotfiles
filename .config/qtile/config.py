f24994c4279e6ceb88d4616d62e1c82f839e3805
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-01-14 15:26:31 -0500

    Enabled a keybind.

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -82,8 +82,8 @@ for i in groups:
             desc="Switch to group {}".format(i.name)),
 
         # mod1 + shift + letter of group = switch to & move focused window to group
-        #Key([mod, "shift"], str(group_key), lazy.window.togroup(i.name, switch_group=True),
-        #    desc="Switch to & move focused window to group {}".format(i.name)),
+        Key([mod, "shift"], str(group_key), lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
 
         # Or, use below if you prefer not to switch to that group.
         # # mod1 + shift + letter of group = move focused window to group
